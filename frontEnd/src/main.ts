cdec3d99cc3beb9de4f44e2225d50bad2f7d28ee
StartEen <155966895+StartEen@users.noreply.github.com>
2025-12-18 13:16:16 +0800

    feat(layout): 添加基础布局组件并集成Ant Design Vue
    - 创建 BasicLayout.vue 文件，定义基本页面结构
    - 在 App.vue 中引入并使用 BasicLayout 组件
    - 配置项目标题为 "Cloud Picture Space"
    - 更改网站图标为 icon.png
    - 安装并注册 Ant Design Vue UI 库
    - 移除原有默认模板和样式代码

--- frontEnd/src/main.ts
+++ frontEnd/src/main.ts
@@ -5,8 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue';
+
 
 const app = createApp(App)
+
+
+
+app.use(Antd)
 
 app.use(createPinia())
 app.use(router)
