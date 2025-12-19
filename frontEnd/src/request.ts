657435ed0c1291d61c0ee4e084ad0a75b09c24ed
StartEen <155966895+StartEen@users.noreply.github.com>
2025-12-19 10:38:03 +0800

    feat(frontend): 引入 Axios 并配置全局请求与响应拦截器
    - 添加 axios 依赖用于 HTTP 请求
    - 创建 Axios 实例并设置基础 URL 和超时时间
    - 配置全局请求拦截器（暂为空）
    - 配置全局响应拦截器以处理未登录状态
    - 在响应拦截器中实现登录检查和重定向逻辑
    - 使用 ant-design-vue 的 message 组件提示用户登录
    - 更新 package.json 和 package-lock.json 以包含新依赖项

---
+++ frontEnd/src/request.ts
@@ -0,0 +1,47 @@
+
+import axios from "axios";
+import {message} from "ant-design-vue";
+
+//创建Axios实例
+const myAxios = axios.create({
+  baseURL: 'http://localhost:8123',
+  timeout: 60000, // 请求超时
+  withCredentials: true,
+});
+
+//全局请求拦截器
+myAxios.interceptors.request.use(
+  (config) => {
+    // 在发送请求之前做些什么
+    return config;
+  },
+  (error) => {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  }
+);
+
+//全局响应拦截器
+
+myAxios.interceptors.response.use(
+  (response) => {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    const {data} =response;
+    //未登录
+    if(data.code === 40100){
+      //不是获取用户信息的请求，
+      //并且用户目前不是已经在用户登录界面，则跳转到登录界面
+      if(!response.request.responseURL.includes('/user/get/login')&&!window.location.pathname.includes('/user/login') ){
+        message.warning('请先登录')
+        window.location.href = `/user/login?redirect=${window.location.href}`
+      }
+    }
+    return response;
+  },
+  (error) => {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  }
+)
