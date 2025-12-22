d3a9e8791b49fcb7c4b03b357c00fbeabb86e361
StartEen <155966895+StartEen@users.noreply.github.com>
2025-12-22 18:08:31 +0800

    feat(picture): 新增图片实体及基础服务接口
    - 创建Picture实体类，包含图片URL、名称、分类等属性
    - 实现PictureMapper数据库操作接口
    - 定义PictureService业务逻辑接口
    - 配置MyBatis Plus注解支持数据库映射
    - 添加序列化支持和删除标记字段
    - 设置创建时间、编辑时间和更新时间字段

---
+++ backEnd/src/main/java/com/cloud/picture/space/backend/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.cloud.picture.space.backend.mapper;
+
+import com.cloud.picture.space.backend.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yz2025120101
+* @description 针对表【picture(图片)】的数据库操作Mapper
+* @createDate 2025-12-22 18:06:56
+* @Entity com.cloud.picture.space.backend.model.entity.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
---
+++ backEnd/src/main/java/com/cloud/picture/space/backend/service/PictureService.java
@@ -0,0 +1,13 @@
+package com.cloud.picture.space.backend.service;
+
+import com.cloud.picture.space.backend.model.entity.Picture;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author yz2025120101
+* @description 针对表【picture(图片)】的数据库操作Service
+* @createDate 2025-12-22 18:06:56
+*/
+public interface PictureService extends IService<Picture> {
+
+}
