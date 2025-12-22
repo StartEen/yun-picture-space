d0cd5761a12a7ca114404d41f627816a3e404037
StartEen <155966895+StartEen@users.noreply.github.com>
2025-12-22 13:42:49 +0800

    feat(backend): 解决Long类型JSON序列化精度丢失问题
    - 添加JsonConfig配置类
    - 注册Long类型ToStringSerializer
    - 解决账号ID传输精度不足问题
    - 配置Spring MVC JSON序列化规则

---
+++ backEnd/src/main/java/com/cloud/picture/space/backend/controller/JsonConfig.java
@@ -0,0 +1,36 @@
+package com.cloud.picture.space.backend.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * @Author: StartEnd
+ * @CreateTime: 2025-12-22  13:35
+ * @Description: TODO Spring MVC Json 配置
+ *
+ * 解决后端传账号id时，精度不够问题
+ */
+@JsonComponent
+public class JsonConfig {
+
+    /**
+     * 添加Long转Json精度丢失问题解决
+     *
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        return objectMapper;
+    }
+
+
+}
