c044eb3965576639755d1dceab4d89cc425b5fad
StartEen <155966895+StartEen@users.noreply.github.com>
2025-12-22 13:31:59 +0800

    feat(config): 配置Mybatis-Plus分页插件
    - 添加 MybatisPlusInterceptor 拦截器配置
    - 启用 MYSQL 数据库的分页功能
    - 引入 mybatis-plus-jsqlparser 依赖
    - 添加 mybatis-plus-bom 依赖管理

---
+++ backEnd/src/main/java/com/cloud/picture/space/backend/config/MybatisPlusConfig.java
@@ -0,0 +1,32 @@
+package com.cloud.picture.space.backend.config;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: StartEnd
+ * @CreateTime: 2025-12-22  13:27
+ * @Description: TODO Mybatis-Plus 配置（拦截器）
+ */
+
+@Configuration
+@MapperScan("com.cloud.picture.space.backend.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 添加分页插件
+     * （拦截器配置）
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
+        // 如果有多数据源可以不配具体类型, 否则都建议配上具体的 DbType
+        return interceptor;
+    }
+}
