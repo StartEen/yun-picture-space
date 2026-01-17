d0fee81a9f96bd720948ea25eaa9ac61f258155c
StartEen <155966895+StartEen@users.noreply.github.com>
2026-01-17 13:44:16 +0800

    feat(space): 添加空间类别枚举类
    - 定义了私有空间和团队空间两种类型
    - 实现了根据值获取枚举项的功能
    - 添加了文本描述和数值映射
    - 集成了hutool工具库进行空值判断
    - 提供了完整的枚举类结构和注释

---
+++ backEnd/src/main/java/com/cloud/picture/space/backend/model/enums/SpaceTypeEnum.java
@@ -0,0 +1,40 @@
+package com.cloud.picture.space.backend.model.enums;
+
+import cn.hutool.core.util.ObjectUtil;
+import lombok.Getter;
+
+/**
+ * @Author: StartEnd
+ * @CreateTime: 2026-01-17  13:41
+ * @Description: TODO 空间类别枚举类
+ */
+@Getter
+public enum SpaceTypeEnum {
+
+    PRIVATE("私有空间", 0),
+    PUBLIC("团队空间", 1);
+
+
+    private final String text;
+    private final int value;
+
+    SpaceTypeEnum(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    /**
+     * 根据value获取枚举项
+     */
+    public static SpaceTypeEnum getEnumByValue(Integer value) {
+        if (ObjectUtil.isNull(value)) {
+            return null;
+        }
+        for (SpaceTypeEnum valueEnum : values()) {
+            if (valueEnum.value == value) {
+                return valueEnum;
+            }
+        }
+        return null;
+    }
+}
