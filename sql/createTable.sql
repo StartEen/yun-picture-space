f907cb2c5992bc9e469403b7f9f670accdbb5295
StartEen <155966895+StartEen@users.noreply.github.com>
2026-01-17 13:27:48 +0800

    feat(database): 添加空间成员关联表
    - 创建 space_user 表用于管理空间与用户的关联关系
    - 添加空间ID和用户ID的唯一索引约束
    - 增加基于空间ID和用户ID的查询索引优化
    - 支持设置用户在空间中的不同角色权限
    - 实现空间成员关系的数据存储结构

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -111,4 +111,18 @@ ALTER TABLE space
 CREATE INDEX idx_spaceType ON space (spaceType);
 
 
+-- 空间成员表
+create table if not exists space_user
+(
+    id         bigint auto_increment comment 'id' primary key,
+    spaceId    bigint                                 not null comment '空间id',
+    userId     bigint                                 not null comment '用户id',
+    spaceRole  varchar(128) default 'viewer'          null comment '空间角色：view/editor/admin',
+    createTime datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    -- 索引设计
+    UNIQUE KEY uk_spaceId_userId (spaceId, userId), -- 唯一索引，用户在一个空间只能有一个角色
+    INDEX idx_spaceId (spaceId),                    -- 提升基于空间ID的查询性能
+    INDEX idx_userId (userId)                       -- 提升基于用户ID的查询性能
+) comment '空间用户关联' collate = utf8mb4_unicode_ci;
 
