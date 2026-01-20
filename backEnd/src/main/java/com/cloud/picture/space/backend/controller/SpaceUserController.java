4edc264a6a11b1e2c5ae9ab12b06580af2910b23
StartEen <155966895+StartEen@users.noreply.github.com>
2026-01-20 08:57:21 +0800

    feat(spaceUser): 添加删除空间用户功能
    - 引入 DeleteRequest 和 SpaceUser 实体类
    - 添加 HttpServletRequest 参数支持
    - 实现 deleteSpaceUser 接口用于删除空间用户
    - 集成参数验证和错误处理逻辑
    - 完善数据库操作和响应返回机制

--- backEnd/src/main/java/com/cloud/picture/space/backend/controller/SpaceUserController.java
+++ backEnd/src/main/java/com/cloud/picture/space/backend/controller/SpaceUserController.java
@@ -2,19 +2,23 @@ package com.cloud.picture.space.backend.controller;
 
 
 import com.cloud.picture.space.backend.common.BaseResponse;
+import com.cloud.picture.space.backend.common.DeleteRequest;
 import com.cloud.picture.space.backend.common.ResultUtils;
 import com.cloud.picture.space.backend.exception.ErrorCode;
 import com.cloud.picture.space.backend.exception.ThrowUtils;
 import com.cloud.picture.space.backend.model.dto.spaceUser.SpaceUserAddRequest;
+import com.cloud.picture.space.backend.model.entity.SpaceUser;
 import com.cloud.picture.space.backend.service.SpaceService;
 import com.cloud.picture.space.backend.service.SpaceUserService;
 import com.cloud.picture.space.backend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author: StartEnd
@@ -45,11 +49,18 @@ public class SpaceUserController {
         return ResultUtils.success(id);
     }
 
-
-
-
-
-
+    @PostMapping("/delete")
+    public BaseResponse<Boolean> deleteSpaceUser(@RequestBody DeleteRequest deleteRequest, HttpServletRequest request) {
+        ThrowUtils.throwIf((deleteRequest == null || deleteRequest.getId() <= 0), ErrorCode.PARAMS_ERROR);
+        long id = deleteRequest.getId();
+        // 判断是否存在
+        SpaceUser oldSpaceUser = spaceUserService.getById(id);
+        ThrowUtils.throwIf(oldSpaceUser == null, ErrorCode.NOT_FOUND_ERROR);
+        // 操作数据库
+        boolean result = spaceUserService.removeById(id);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        return ResultUtils.success(true);
+    }
 
 
 }
