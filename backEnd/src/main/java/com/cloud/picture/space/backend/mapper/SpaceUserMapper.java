a5531f43d43797a720ff9f60a9bcf3a5befdde41
StartEen <155966895+StartEen@users.noreply.github.com>
2026-01-27 10:44:16 +0800

    feat(mapper): 添加空间用户关联表的数据库操作映射器
    - 创建了 SpaceUserMapper 接口继承 BaseMapper
    - 配置了针对 space_user 表的数据库操作功能
    - 添加了实体类 SpaceUser 的映射关系
    - 完成了基础的 CRUD 操作支持

---
+++ backEnd/src/main/java/com/cloud/picture/space/backend/mapper/SpaceUserMapper.java
@@ -0,0 +1,18 @@
+package com.cloud.picture.space.backend.mapper;
+
+import com.cloud.picture.space.backend.model.entity.SpaceUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yz2025120101
+* @description 针对表【space_user(空间用户关联)】的数据库操作Mapper
+* @createDate 2026-01-17 14:15:30
+* @Entity com.cloud.picture.space.backend.model.entity.SpaceUser
+*/
+public interface SpaceUserMapper extends BaseMapper<SpaceUser> {
+
+}
+
+
+
+
