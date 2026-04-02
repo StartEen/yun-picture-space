b936d72ccc94bb77896f2da3aabee0ecce91cd16
StartEen <155966895+StartEen@users.noreply.github.com>
2026-04-02 10:07:38 +0800

    feat(analyze): 添加空间分析功能组件
    - 新增 SpaceCategoryAnalyze、SpaceRankAnalyze、SpaceSizeAnalyze、SpaceTagAnalyze、SpaceUsageAnalyze、SpaceUserAnalyze 分析组件
    - 完善 SpaceAnalyzePage 页面实现空间分析功能
    - 添加路由参数解析和用户权限判断逻辑
    - 实现空间 ID 查询和管理员身份验证功能
    - 添加页面标题显示和链接跳转功能
    - 集成登录用户状态管理 store

---
+++ frontEnd/src/components/analyze/SpaceSizeAnalyze.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="xxxPage">
+    <!-- 组件内容 -->
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- frontEnd/src/pages/analyze/SpaceAnalyzePage.vue
+++ frontEnd/src/pages/analyze/SpaceAnalyzePage.vue
@@ -1,9 +1,54 @@
 <template>
-  <div id="xxxPage">
-    <!-- 组件内容 -->
+  <div id="spaceAnalyzePage">
+    <h2>
+      空间图库分析 -
+      <span v-if="queryAll">全部空间</span>
+      <span v-else-if="queryPublic">公共图库</span>
+      <span v-else>
+        <a :href="`/space/${spaceId}`" target="_blank">空间 id：{{ spaceId }}</a>
+      </span>
+    </h2>
+    <div style="margin-bottom: 16px" />
+
+
+
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+import {computed} from "vue";
+import {useLoginUserStore} from "@/stores/useLoginUserStore.ts";
 
-<style scoped></style>
+const route = useRoute()
+
+// 空间 id
+const spaceId = computed(() => {
+  return route.query?.spaceId as string
+})
+
+// 判断用户是否为管理员
+const loginUserStore = useLoginUserStore()
+const loginUser = loginUserStore.loginUser
+const isAdmin = computed(() => {
+  return loginUser.userRole === 'admin'
+})
+
+// 是否查询所有空间
+const queryAll = computed(() => {
+  return !!route.query?.queryAll
+})
+
+// 是否查询公共空间
+const queryPublic = computed(() => {
+  return !!route.query?.queryPublic
+})
+
+
+</script>
+
+<style scoped>
+#spaceAnalyzePage {
+  margin-bottom: 16px;
+}
+</style>
